274821a749de31c10b929141109593ed57c82b96
Coding Hub <>
2021-05-31 09:09:08 +0000

    Added Post command on bot's dm.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os 
 import discord
+import datetime
 import random
 from discord.ext import commands
 from keep_alive import keep_alive
@@ -28,6 +29,17 @@ class Greetings(commands.Cog):
       await ch.send("🥺 Bye **{0.name}**".format(member))
       
 bot.add_cog(Greetings(bot))
-      
+
+@bot.command(aliases=['profile'])
+@commands.dm_only()
+async def post(ctx,id:int,*,url:str):
+  ch = bot.get_channel(848832656468344832)
+  em1 = discord.Embed(colour=discord.Colour.gold())
+  em1.set_author(name=ctx.author.name,icon_url=ctx.author.avatar_url)
+  em1.description = f'Player in-game id {id}'
+  em1.set_image(url=url)
+  em1.timestamp = datetime.datetime.utcnow()
+  await ch.send(embed=em1)
+  
 keep_alive()
 bot.run(os.environ['token'])
