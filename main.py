b093116d645f7d86b83e453e00ad13b8af2f74d2
Coding Hub <>
2021-05-31 04:43:14 +0000

    Bot has been assigned. You can use  or @mention command

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import os 
+import discord
+from discord.ext import commands
 from keep_alive import keep_alive
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
+
 keep_alive()
+bot.run(os.environ['token'])
